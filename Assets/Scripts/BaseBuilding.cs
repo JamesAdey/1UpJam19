4d41c403f1251f2d0dc7344331749c27a51d747c
JamesAdey <jamesadey91@gmail.com>
2019-06-14 20:36:31 +0100

    base building

---
+++ Assets/Scripts/BaseBuilding.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseBuilding : MonoBehaviour
+{
+    public float blockingRadius;
+    public List<NavNode> nodes;
+}
