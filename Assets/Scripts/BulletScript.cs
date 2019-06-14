9913bce42d66ffab6fe5381ce968f643b38bac6a
sxg661 <sxg661@student.bham.ac.uk>
2019-06-14 17:33:43 +0100

    bullet

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position);
+    }
+}
