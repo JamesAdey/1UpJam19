8fc8ae56e9ac506e063468e3cdd4d05ba3f1226c
JamesAdey <jamesadey91@gmail.com>
2019-06-14 17:32:40 +0100

    player working

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerInput
 {
+    public Vector3 walkDir;
+    public Vector3 strafeDir;
     public float strafeInput;
     public float walkInput;
     public Vector3 lookPos;
+    
 }
