8e30dd0c2a3513fe6b9f4c0db13cf2653af4ccf5
JamesAdey <jamesadey91@gmail.com>
2019-06-15 19:56:51 +0100

    partial wizard build

--- Assets/Scripts/EnemyBot/CB_WizardBuild.cs
+++ Assets/Scripts/EnemyBot/CB_WizardBuild.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ContextBehaviourAI;
+using System;
 
 public class CB_WizardBuild : ContextBehaviour<float>
 {
+
+    int totalEnemyBuildings = 0;
+    int totalMyBuildings = 0;
+    bool wantBuild = false;
+
     public override void Process(ContextMap<float> map)
     {
         throw new System.NotImplementedException();
     }
 
-    void Process(MovementCMap movementMap, KeypressCMap keyboardMap, PlayerController player)
+    void Process(MovementCMap movementMap, KeypressCMap keyboardMap, PlayerController controller)
     {
+        PlayerData enemyPlayer = GameManager.manager.GetOpposingPlayer(controller.team);
+        PlayerData myPlayer = GameManager.manager.GetPlayer(controller.team);
+
         // wizard build
         // MOVE near build spot
+
+        totalEnemyBuildings = enemyPlayer.buildings.Count;
+        totalMyBuildings = myPlayer.buildings.Count;
+        
+        if(totalEnemyBuildings >= totalMyBuildings)
+        {
+            wantBuild = true;
+        }
+        else
+        {
+            wantBuild = false;
+        }
 
         // Press keys to create buildings
     }
