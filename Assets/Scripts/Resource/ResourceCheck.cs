be9ea94a7f8ecc1a7b44bcb9ac6fff707f33a330
sxg661 <sxg661@student.bham.ac.uk>
2019-06-16 11:27:42 +0100

    madenicegui

---
+++ Assets/Scripts/Resource/ResourceCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceCheck : MonoBehaviour
+{
+    [SerializeField]
+    Teams.Team team;
+
+    [SerializeField]
+    int cost;
+
+    PlayerData myPlayer;
+    Button myButton;
+
+
+    private void Start()
+    {
+        myPlayer = GameManager.manager.GetPlayer(team);
+        myButton = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myPlayer.resources < cost)
+        {
+            myButton.interactable = false;
+        }
+        else
+        {
+            myButton.interactable = true;
+        }
+    }
+}
