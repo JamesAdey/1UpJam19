74b6dbd0f1403a11c944243da49396b0b4a4b5e7
sxg661 <sxg661@student.bham.ac.uk>
2019-06-22 12:32:09 +0100

    nearly finished attack behav

--- Assets/Scripts/AltBotBehavs/AttackBehav.cs
+++ Assets/Scripts/AltBotBehavs/AttackBehav.cs
@@ -8,12 +8,14 @@ public class AttackBehav : BaseBehav
     {
     }
 
+    private float mySqShootRange = 400;
+
     /// <summary>
     /// Returns the best building to shoot and its score.
     /// </summary>
     /// <param name="enemyPlayer"></param>
     /// <returns></returns>
-    private (BaseBuilding, int) FindBestBuilding(PlayerData enemyPlayer)
+    private (BaseBuilding, int) FindBestBuilding()
     {
         BaseBuilding bestBuilding = null;
         int bestScore = -1;
@@ -25,7 +27,7 @@ public class AttackBehav : BaseBehav
             float sqDist = (playerTrans.position - building.GetPosition()).sqrMagnitude;  
 
             //if it's in range, then we'll give it a score
-            if ( sqDist < 900 )
+            if ( sqDist < mySqShootRange )
             {
                 float sqDistFromEnemyTower = (building.GetPosition() - enemyPlayer.mainTower.GetPosition()).sqrMagnitude;
                 int myScore = 0;
@@ -41,12 +43,13 @@ public class AttackBehav : BaseBehav
                 switch(building.GetBuildingType())
                 {
                     case BuildingType.MAIN:
-                        myScore += 2;
+                        myScore += 3;
                         break;
                     case BuildingType.BARRACKS:
-                        myScore += 1;
+                        myScore += 2;
                         break;
                     case BuildingType.TOWER:
+                        myScore += 1;
                         break;
                 }
 
@@ -65,51 +68,108 @@ public class AttackBehav : BaseBehav
     {
         int myScore = -1;
 
-        if ((enemyPlayer.controller.transform.position - playerTrans.position).sqrMagnitude < 900)
+        if ((enemyPlayer.controller.transform.position - playerTrans.position).sqrMagnitude < mySqShootRange)
         {
             myScore += 4;
 
             if (enemyPlayer.controller.inputs.primaryAttack)
             {
-                myScore += 3;
+                myScore += 4;
             }
         }
 
         return myScore;
-
-        
     }
+
+    public (Minion, int) FindBestMinion()
+    {
+        Minion bestMinion = null;
+        int bestScore = -1;
+
+        foreach(Minion minion in enemyPlayer.minions)
+        {
+            int myScore = 0;
+
+            if((playerTrans.position - minion.Position).sqrMagnitude < 1)
+            {
+                myScore += 7;
+            }
+            else
+            {
+                myScore += 0;
+            }
+
+            if(myScore > bestScore)
+            {
+                bestMinion = minion;
+                bestScore = myScore;
+            }
+        }
+
+        return (bestMinion, bestScore);
+    }
+
+
+    private (Vector3, int) FindBest(List<(Vector3, int)> pairs)
+    {
+        (Vector3, int) currentBest = (Vector3.zero, -1);
+
+        foreach((Vector3, int) pair in pairs)
+        {
+            if(pair.Item2 > currentBest.Item2){
+                currentBest = pair;
+            }
+        }
+
+        return currentBest;
+    }
+
+    private (Vector3, int) FindTargetPair((BaseBuilding, int) building, int playerScore, (Minion, int) bestMinion)
+    {
+        List<(Vector3, int)> pairs = new List<(Vector3, int)>();
+
+        if (building.Item1 != null)
+        {
+            pairs.Add((building.Item1.GetPosition(), building.Item2));
+        }
+
+        if (playerScore > -1)
+        {
+            pairs.Add((enemyPlayer.controller.transform.position, playerScore));
+        }
+
+        if (bestMinion.Item1 != null)
+        {
+            pairs.Add((bestMinion.Item1.Position, bestMinion.Item2));
+        }
+
+        (Vector3, int) pair = FindBest(pairs);
+        return pair;
+    }
+
+
 
     public override void Think()
     {
-
-        //THIS SHOULD NOT BE HARD CODED
-        PlayerData player = GameManager.manager.GetOpposingPlayer(Teams.Team.AI);
-
         //find nearest building
-        (BaseBuilding, int) building = FindBestBuilding(player);
+        (BaseBuilding, int) building = FindBestBuilding();
 
         //check the status and distance of the player
         int playerScore = CheckEnemyPlayer();
 
-        
-        //decides where to attack
+        (Minion, int) bestMinion = FindBestMinion();
+        (Vector3, int) pair = FindTargetPair(building, playerScore, bestMinion);
 
-        inputs.primaryAttack = true;
-
-        if (playerScore > building.Item2)
+        if (pair.Item2 != -1)
         {
-            inputs.lookPos = player.controller.transform.position;
-        }
-        else if(playerScore < building.Item2)
-        {
-            inputs.lookPos = building.Item1.GetPosition();
+            inputs.lookPos = pair.Item1;
+            inputs.primaryAttack = true;
         }
         else
         {
             inputs.primaryAttack = false;
         }
 
-        
     }
+
 }
